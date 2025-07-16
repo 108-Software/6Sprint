f00f1152caa07129ddf085998396c17c983edbb7
108-Software <75811746+108-Software@users.noreply.github.com>
2025-07-16 20:18:16 +0300

    Update handlers.go
    Attemmpt 6

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -81,7 +81,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte(converted))
 
 }
