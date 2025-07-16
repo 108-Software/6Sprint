e005815cf23075453fc77f67c24ea080c5ecab90
108-Software <zaginyko@mail.ru>
2025-07-16 19:37:13 +0300

    Почему не Gin

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,40 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func AutoDetectAndConvert(input string) (string, error) {
+	if strings.TrimSpace(input) == "" {
+		return "", errors.New("empty input")
+	}
+
+	if isMorseCode(input) {
+		return morse.ToText(input), nil
+	}
+	return morse.ToMorse(input), nil
+}
+
+func isMorseCode(s string) bool {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return false
+	}
+
+	allowedChars := map[rune]bool{
+		'.': true,
+		'-': true,
+		' ': true,
+		'/': true,
+	}
+
+	for _, r := range s {
+		if !allowedChars[r] {
+			return false
+		}
+	}
+	return true
+}
